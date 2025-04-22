b86f5f5f220d1c398b114d30eeed607a5a429f20
96mdjanealam <96mdjanealam@gmail.com>
2025-04-22 16:06:08 +0600

    user controller updating

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 // Register User: /api/user/register
 export const register = async (req, res) => {
@@ -10,13 +11,14 @@ export const register = async (req, res) => {
       return res.json({ success: false, message: "missing details" });
     }
 
-    const existingUser = await User.findOne({email})
+    const existingUser = await User.findOne({ email });
 
-    if(existingUser){
-        return res.json({success:false, message:"User already exists"})
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
     }
 
-    const hashedPassword = await bcrypt
-
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = User.create({ name, email, password: hashedPassword });
+    const token = jwt.sign({ id: user._id });
   } catch (error) {}
 };
