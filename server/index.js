aa211dc7914dc72797620b53f55bb3e56c8155cf
96mdjanealam <96mdjanealam@gmail.com>
2025-05-04 17:32:02 +0600

    complete

--- server/index.js
+++ server/index.js
@@ -19,10 +19,12 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://earthy-harvest-client.vercel.app",
+];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
-
 
 // Middleware configuration
 app.use(express.json());
